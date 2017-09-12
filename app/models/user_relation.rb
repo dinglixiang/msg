a1dbc2e0093b1a9d9dc34384be1fea75d98e3a85
Lixiang Ding <zhimadlx@gmail.com>
2017-09-12 13:01:56 +0800

    Create user_relation table and model

---
+++ app/models/user_relation.rb
@@ -0,0 +1,2 @@
+class UserRelation < ApplicationRecord
+end
---
+++ db/migrate/20170912044452_create_user_relations.rb
@@ -0,0 +1,11 @@
+class CreateUserRelations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_relations do |t|
+      t.integer :user_id,   null: false, default: 0, comment: '当前用户ID'
+      t.integer :friend_id, null: false, default: 0, comment: '当前用户ID'
+      t.integer :status,    null: false, default: 0, comment: '好友状态, 0 好友 1 已删除'
+    end
+
+    add_index :user_relations, [:user_id, :status]
+  end
+end
