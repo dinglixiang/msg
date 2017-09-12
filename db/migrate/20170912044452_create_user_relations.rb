f0cd98b8c2396b7a88f2190a326ba5646cde5f09
Lixiang Ding <zhimadlx@gmail.com>
2017-09-12 23:44:19 +0800

    Change the status column size

--- db/migrate/20170912044452_create_user_relations.rb
+++ db/migrate/20170912044452_create_user_relations.rb
@@ -3,7 +3,7 @@ class CreateUserRelations < ActiveRecord::Migration[5.1]
     create_table :user_relations do |t|
       t.integer :user_id,   null: false, default: 0, comment: '用户ID'
       t.integer :friend_id, null: false, default: 0, comment: '联系人ID'
-      t.integer :status,    null: false, default: 0, comment: '好友状态, 0 好友 1 已删除'
+      t.integer :status,    null: false, default: 0, limit: 1, comment: '好友状态, 0 好友 1 已删除'
 
       t.timestamps
     end
--- db/migrate/20170912050236_create_messages.rb
+++ db/migrate/20170912050236_create_messages.rb
@@ -5,7 +5,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.integer :friend_id,   null: false, default: 0, comment: '接收者ID'
       t.integer :sender_id,   null: false, default: 0, comment: '发送者ID'
       t.integer :reciever_id, null: false, default: 0, comment: '接受者ID'
-      t.integer :status,      null: false, default: 0, comment: '0 未读，1 已读，2 删除'
+      t.integer :status,      null: false, default: 0, limit: 1, comment: '0 未读，1 已读，2 删除'
       t.text    :content,     null: false, comment: '消息内容'
 
       t.timestamps
